16eb304793c0c7a629ce2b1e977e41147835e06b
Bryan Wain <bryandwain@gmail.com>
2013-12-10 23:14:42 -0600

    finished initialize method

--- lib/date_calculator.rb
+++ lib/date_calculator.rb
@@ -7,5 +7,20 @@ class DateCalculator
     rescue ArgumentError
       raise "Invalid Date. Use form MM/DD/YYYY"
     end
+
+    case interval
+    when "daily"
+      @interval = :daily
+    when "weekly"
+      @interval = :weekly
+    when "bi-weekly"
+      @interval = :bi_weekly
+    when "semi-monthly"
+      @interval = :semi_monthly
+    when "monthly"
+      @interval = :monthly
+    else
+      raise "Invalid Interval. Please use daily, weekly, bi-weekly, semi-monthly or monthly"
+    end
   end
 end
--- spec/date_calculator_spec.rb
+++ spec/date_calculator_spec.rb
@@ -3,9 +3,14 @@ require 'date_calculator'
 describe DateCalculator do
   describe "#initialize" do
     context "when date is not of the form MM/DD/YYYY" do
-      let(:date) {"05/123/19"}
-      specify do
-        expect{DateCalculator.new(date, "weekly")}.to raise_error("Invalid Date. Use form MM/DD/YYYY")
+      it "raises an error" do
+        expect{DateCalculator.new("05/123/19", "weekly")}.to raise_error("Invalid Date. Use form MM/DD/YYYY")
+      end
+    end
+
+    context "when interval is not daily, weekly, bi-weekly, semi-monthly or monthly" do
+      it "raises an error" do
+        expect{DateCalculator.new("05/15/2013", "bad-interval")}.to raise_error("Invalid Interval. Please use daily, weekly, bi-weekly, semi-monthly or monthly")
       end
     end
   end
