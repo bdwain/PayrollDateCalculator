86d46f1e5cba91c387e4c667e58bcc0df9353edf
Bryan Wain <bryandwain@gmail.com>
2013-12-12 15:00:14 -0600

    don't need to initialize with date after all

--- spec/date_calculator_spec.rb
+++ spec/date_calculator_spec.rb
@@ -2,12 +2,6 @@ require 'date_calculator'
 
 describe DateCalculator do
   describe "#initialize" do
-    context "when date is not of the form MM/DD/YYYY" do
-      it "raises an error" do
-        expect{DateCalculator.new("05/123/19", "weekly")}.to raise_error("Invalid Date. Use form MM/DD/YYYY")
-      end
-    end
-
     context "when interval is not daily, weekly, bi-weekly, semi-monthly or monthly" do
       it "raises an error" do
         expect{DateCalculator.new("05/15/2013", "bad-interval")}.to raise_error("Invalid Interval. Please use daily, weekly, bi-weekly, semi-monthly or monthly")
