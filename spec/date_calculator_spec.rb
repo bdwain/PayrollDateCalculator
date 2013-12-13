e004f79dcc1f5e8f490e90e19b1186a8ce71dba7
Bryan Wain <bryandwain@gmail.com>
2013-12-13 15:08:00 -0600

    fix this mistake from last commit

--- spec/date_calculator_spec.rb
+++ spec/date_calculator_spec.rb
@@ -4,7 +4,7 @@ describe DateCalculator do
   describe "#initialize" do
     context "when interval is not daily, weekly, bi-weekly, semi-monthly or monthly" do
       it "raises an error" do
-        expect{DateCalculator.new(Date.new(2013, 1, 1), "bad-interval")}.to raise_error("Invalid Interval. Please use daily, weekly, bi-weekly, semi-monthly or monthly")
+        expect{DateCalculator.new("bad-interval")}.to raise_error("Invalid Interval. Please use daily, weekly, bi-weekly, semi-monthly or monthly")
       end
     end
   end
