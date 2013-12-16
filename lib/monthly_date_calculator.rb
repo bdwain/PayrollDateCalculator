e0c9e93da0f74857271f44cd9b78d26262d0f4bf
Bryan Wain <bryandwain@gmail.com>
2013-12-16 00:51:20 -0600

    shorten an unnecssarily jong range

--- lib/monthly_date_calculator.rb
+++ lib/monthly_date_calculator.rb
@@ -11,7 +11,7 @@ class MonthlyDateCalculator < DateCalculator
     #end the range here instead of end_date so that any payday failling between end_date and this will be included, which will then get paid inside start_date...end_date
     first_valid_payday_outside_range = get_next_valid_paydate(end_date)
 
-    result = (start_date...first_valid_payday_outside_range).to_a.select! {|day| is_corresponding_paydate?(valid_days, day)}
+    result = (start_date..first_valid_payday_outside_range).to_a.select! {|day| is_corresponding_paydate?(valid_days, day)}
     result.map! {|day| get_last_valid_paydate(day)}
     result.keep_if {|day| (start_date...end_date).cover?(day) }    
   end
